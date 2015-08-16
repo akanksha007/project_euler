a09d6748efc97d758e352bb736a6c8f4b5b197c3
akanksha007 <akankshaagarwal007@gmail.com>
2015-08-16 16:14:22 +0530

    file added

---
+++ sum_square_difference.py
@@ -0,0 +1,9 @@
+"""
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+n=100
+square_sum=(n*(n+1)/2)**2
+sum_square=n*(n+1)*(2*n+1)/6
+print square_sum-sum_square
+
+
